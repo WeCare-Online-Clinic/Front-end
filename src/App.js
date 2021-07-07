923378fff44b73fdbc12e16ec92c7d860c216cb9
Your Name <sasindusubodhaka@gmail.com>
2021-07-07 23:40:41 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/index'
 import Auth from './authStore/Auth'
---
+++ src/components/pages/Actors/Admin/AddDoctors.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FormGroup } from '@material-ui/core';
+function AddDoctors() {
+    return (
+        <div>
+            <FormGroup />
+        </div>
+    )
+}
+
+export default AddDoctors
