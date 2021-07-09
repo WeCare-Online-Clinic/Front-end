eb4d22ffc32e49d5633b48d2235f359efc03e7fe
Aveeshajay <aveeeshajayawardena12@gmail.com>
2021-07-09 12:30:11 -0700

    Head Nurse View Pages

---
+++ src/components/Table/PatientQueueTable.js
@@ -0,0 +1,191 @@
+import React, { useState } from 'react'
+import clsx from 'clsx'
+import PropTypes from 'prop-types'
+import PerfectScrollbar from 'react-perfect-scrollbar'
+import { makeStyles } from '@material-ui/styles'
+import Typography from '@material-ui/core/Typography'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TablePagination,
+  Button,
+  AppBar,
+  Grid,
+  TextField,
+} from '@material-ui/core'
+import { QueueData } from './QueueData'
+import PageviewIcon from '@material-ui/icons/Pageview'
+import { useHistory } from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: 0,
+  },
+  content: {
+    padding: 0,
+  },
+  inner: {
+    minWidth: 'inherit',
+  },
+  nameContainer: {
+    display: 'flex',
+    alignItems: 'baseline',
+  },
+  status: {
+    marginRight: 15,
+  },
+  actions: {
+    justifyContent: 'flex-end',
+  },
+  cell: {
+    color: '#4e4f50',
+    fontSize: '16px',
+  },
+  search_items: {
+    maxHeight: '50px',
+    margin: '10px',
+    backgroundColor: '#fff',
+  },
+  grid: {
+    borderBottom: '2px solid #3f51b5',
+    padding: '5px',
+  },
+}))
+
+const PatientQueueTable = (props) => {
+  const history = useHistory()
+  const { className } = props
+  const [rowsPerPage, setRowsPerPage] = useState(10) // set no.of rows per page
+  const [page, setPage] = useState(0) // set page no
+
+  const tableHeaders = [
+    // add table header names
+    { text: 'Patient ID' },
+    { text: 'Patient Name' },
+    { text: 'Queue Number' },
+    { text: 'Clinic Date' },
+  ]
+
+  const classes = useStyles()
+
+  const handlePageChange = (event, page) => {
+    setPage(page)
+  }
+  const handleRowsPerPageChange = (event) => {
+    setRowsPerPage(event.target.value)
+  }
+
+  return (
+    <div>
+      <Card padding={'0'} className={clsx(classes.root, className)}>
+        <Grid className={classes.grid} container justify='space-around'>
+          <Grid item sm></Grid>
+          <Grid
+            item
+            alignContent='center'
+            style={{ backgroundColor: '#3f51b5', borderRadius: '5px' }}
+          >
+            <form clasName={classes.root}>
+              <TextField
+                className={classes.search_items}
+                label='Patient Name'
+                variant='outlined'
+                size='small'
+              ></TextField>
+              <TextField
+                className={classes.search_items}
+                label='Patient ID'
+                variant='outlined'
+                size='small'
+              ></TextField>
+              <Button
+                startIcon={<PageviewIcon />}
+                variant='contained'
+                size='large'
+                color='secondary'
+                style={{ margin: '10px' }}
+              >
+                Search
+              </Button>
+            </form>
+          </Grid>
+        </Grid>
+        <CardContent className={classes.content}>
+          <PerfectScrollbar>
+            <div className={classes.inner}>
+              <Table>
+                <TableHead
+                  style={{ backgroundColor: '#ebf5f7' }}
+                  className={classes.head}
+                >
+                  <TableRow>
+                    {tableHeaders.map((col) => (
+                      <TableCell
+                        style={{
+                          color: '#3f51b5',
+                          fontWeight: 'bold',
+                          fontSize: '16px',
+                          borderBottom: '1px solid #000',
+                        }}
+                        className={classes.hoverable}
+                      >
+                        <span>{col.text}</span>
+                      </TableCell>
+                    ))}
+                    <TableCell
+                      style={{ borderBottom: '1px solid #000' }}
+                    ></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {QueueData
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) // slice patienData array to no.of rows per page
+                    .map(
+                      (
+                        patient // add table row of QueueData
+                      ) => (
+                        <TableRow className={classes.tableRow} hover>
+                          <TableCell className={classes.cell}>
+                            {patient.id}
+                          </TableCell>
+                          <TableCell className={classes.cell}>
+                            {patient.name}
+                          </TableCell>
+                          <TableCell className={classes.cell}>
+                            {patient.queueno}
+                          </TableCell>
+                          <TableCell className={classes.cell}>
+                            {patient. clinic_date}
+                          </TableCell>
+                        </TableRow>
+                      )
+                    )}
+                </TableBody>
+              </Table>
+            </div>
+          </PerfectScrollbar>
+        </CardContent>
+        <CardActions className={classes.actions}>
+          <TablePagination
+            component='div'
+            count={QueueData.length} // size of QueueData array
+            onChangePage={handlePageChange}
+            onChangeRowsPerPage={handleRowsPerPageChange}
+            page={page}
+            rowsPerPage={rowsPerPage}
+            rowsPerPageOptions={[5, 8, 10, 15]}
+          />
+        </CardActions>
+      </Card>
+    </div>
+  )
+}
+
+export default PatientQueueTable
