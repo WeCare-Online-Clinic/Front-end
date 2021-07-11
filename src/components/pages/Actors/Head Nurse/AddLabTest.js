5081a73856631f658210e31f7659c03631f2cd52
Aveeshajay <aveeeshajayawardena12@gmail.com>
2021-07-11 08:11:18 -0700

    Headnurse pages

---
+++ src/components/pages/Actors/Head Nurse/AddLabTest.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import Layout from '../../../Layout'
+import Header from '../../../Header'
+import Footer from '../../../Footer'
+import Sidebar from '../../../Sidebar/Sidebar'
+import BioChem from '../../../Lab Forms/BioChem'
+import { headnurseMenuItems } from '../../../Sidebar/menuItem'
+import { Grid, makeStyles } from '@material-ui/core'
+import PageviewIcon from '@material-ui/icons/Pageview'
+import { Button } from '@material-ui/core'
+import { TextField } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  dataCard: {
+    backgroundColor: '#fff',
+    borderRadius: '5px',
+    margin: '20px',
+    marginBottom: '0px',
+  },
+  root: {
+    padding: 0,
+  },
+
+  search_items: {
+    maxHeight: '50px',
+    margin: '10px',
+    backgroundColor: '#fff',
+  },
+
+})
+
+function AddLabTest() {
+  return (
+    <Layout
+      header={<Header user='Mr. Mahesh Withanage' />}
+      sidebar={<Sidebar menuItems={headnurseMenuItems} />}
+      footer={<Footer />}
+      content={
+        <div style={{ padding: '20px', backgroundColor: '#ebf5f7' }}>
+          <Content />
+        </div>
+      }
+    ></Layout>
+  )
+}
+
+function Content() {
+  const classes = useStyles()
+  return (
+    <Grid container style={{ padding: '20px' }} spacing={5}>
+      
+      <Grid className={classes.dataCard} item sm={12}>
+      <h3 style={{ color: '#000000' }} align='center'>Add Lab Test</h3>  
+      <Grid className={classes.grid} container justify='space-around'>
+          <Grid item sm></Grid>
+          <Grid
+            item
+            alignContent='center'
+            style={{ backgroundColor: '#3f51b5', borderRadius: '5px' }}
+          >
+            <form clasName={classes.root}>
+              <TextField
+                className={classes.search_items}
+                label='Report Type'
+                variant='outlined'
+                size='small'
+              ></TextField>
+              <Button
+                startIcon={<PageviewIcon />}
+                variant='contained'
+                size='large'
+                color='secondary'
+                style={{ margin: '10px' }}
+              >
+                Search
+              </Button>
+            </form>
+          </Grid>
+        </Grid>
+        <BioChem />
+      </Grid>       
+    </Grid>
+   
+  )
+}
+
+
+export default AddLabTest
---
+++ src/components/pages/Actors/Head Nurse/ManageRequest.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import Layout from '../../../Layout'
+import Header from '../../../Header'
+import Footer from '../../../Footer'
+import Sidebar from '../../../Sidebar/Sidebar'
+import { headnurseMenuItems } from '../../../Sidebar/menuItem'
+import { Grid, makeStyles } from '@material-ui/core'
+import PatientQueueTable from '../../../Table/PatientQueueTable'
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  dataCard: {
+    backgroundColor: '#fff',
+    borderRadius: '5px',
+    margin: '20px',
+    marginBottom: '0px',
+  },
+})
+
+function ManageRequest() {
+  return (
+    <Layout
+      header={<Header />}
+      sidebar={<Sidebar menuItems={headnurseMenuItems} />}
+      footer={<Footer />}
+      content={
+        <div style={{ padding: '20px', backgroundColor: '#ebf5f7' }}>
+          <Content />
+        </div>
+      }
+    ></Layout>
+  )
+}
+
+function Content() {
+  const classes = useStyles()
+  return (
+    <Grid container style={{ padding: '20px' }} spacing={5}>
+      <Grid className={classes.dataCard} item sm={12}>
+        <Grid container>
+          <Grid item>
+            <h3 style={{ color: '#3f51b5' }}>10.30 A.M</h3>
+          </Grid>
+          <Grid item sm></Grid>
+          <Grid item>
+            <h3 style={{ color: '#3f51b5' }}>Welcome Ms.Perera</h3>
+          </Grid>
+        </Grid>
+      </Grid>
+     
+      <Grid className={classes.dataCard} item sm={12}>
+      <h3 style={{ color: '#000000' }} align='center'>Manage Request</h3>
+      <div width="250px">
+      <Button variant="contained" color="primary" style={{ "min-height": "56px", width: "20%" }}>Change</Button>
+      </div>
+      <PatientQueueTable />
+          </Grid>
+     
+          
+    </Grid>
+   
+  )
+}
+
+
+export default ManageRequest
